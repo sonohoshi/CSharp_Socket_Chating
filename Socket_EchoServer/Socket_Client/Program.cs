3d9b3424a5cf34ff3e4abc2eded371de3b4842d2
sonohoshi <yd111544@gmail.com>
2020-10-28 23:08:02 +0900

    Init New Project

--- Socket_EchoServer/Socket_Client/Program.cs
+++ Socket_EchoServer/Socket_Client/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Socket_Client
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Socket_Client
 {
+    public class StateObject
+    {
+        public const int BufferSize = 1024;
+        public byte[] Buffer = new byte[BufferSize];
+        public StringBuilder sBuilder = new StringBuilder();
+        public Socket workSocket = null;
+    }
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
         }
     }
 }
--- Socket_EchoServer/Socket_EchoServer/Program.cs
+++ Socket_EchoServer/Socket_EchoServer/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Socket_EchoServer
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Socket_EchoServer
 {
+    public class StateObject
+    {
+        public const int BufferSize = 1024;
+        public byte[] Buffer = new byte[BufferSize];
+        public StringBuilder SBuilder = new StringBuilder();
+        public Socket WorkSocket = null;
+    }
+    
     internal class Program
     {
+        private const int port = 6974; 
+        
         public static void Main(string[] args)
         {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndpoint = new IPEndPoint(ipAddress, port);
+            
         }
     }
 }
